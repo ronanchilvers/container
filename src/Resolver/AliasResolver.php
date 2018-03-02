6514086d62168a47a8fa3be74f2d71e7ceaa41ff
Ronan Chilvers <ronan@d3r.com>
2018-03-02 23:00:29 +0000

    Add resolver for handling service aliases

---
+++ src/Resolver/AliasResolver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ronanchilvers\Container\Resolver;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Resolver for definitions that are aliases for other services
+ *
+ * @author Ronan Chilvers <ronan@d3r.com>
+ */
+class AliasResolver implements ResolverInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function supports($definition)
+    {
+        return is_string($definition) && '@' == substr($definition, 0, 1);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function resolve(ContainerInterface $container, $definition)
+    {
+        $definition = substr($definition, 1);
+        return $container->get($definition);
+    }
+}
