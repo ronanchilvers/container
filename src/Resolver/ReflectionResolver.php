22f4f8d743745873958454f0fa34ed66b922ecd6
Ronan Chilvers <ronan@d3r.com>
2018-03-02 23:00:53 +0000

    Add resolver using reflection for basic autowiring

---
+++ src/Resolver/ReflectionResolver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Ronanchilvers\Container\Resolver;
+
+use Psr\Container\ContainerInterface;
+use ReflectionClass;
+
+/**
+ * A resolver that uses reflection to try to autowire services
+ *
+ * @author Ronan Chilvers <ronan@d3r.com>
+ */
+class ReflectionResolver implements ResolverInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function supports($definition)
+    {
+        return is_string($definition) && class_exists($definition);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function resolve(ContainerInterface $container, $definition)
+    {
+        if (!is_string($definition) || !class_exists($definition)) {
+            return false;
+        }
+
+        $reflection = new ReflectionClass($definition);
+        if (!$reflection->isInstantiable()) {
+            return false;
+        }
+
+        $constructor = $reflection->getConstructor();
+        if (is_null($constructor)) {
+            return $reflection->newInstance();
+        }
+
+        $dependencies = [];
+        foreach ($constructor->getParameters() as $parameter) {
+            $class = $parameter->getClass();
+            if (is_null($class)) {
+                return false;
+            }
+            $dependencies[] = $container->get($class->getName());
+        };
+
+        return $reflection->newInstanceArgs($dependencies);
+    }
+}
